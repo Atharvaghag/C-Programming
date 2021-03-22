c12ec8205a6bf1e96d8bafdf129d119acccdb32e
Atharva Ghag <77572796+Atharvaghag@users.noreply.github.com>
2021-03-22 14:12:47 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,24 @@
+/*
+    Programme to multiply 2 numbers
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int num1=0;
+    int num2=0;
+    int mul=0;
+    
+    printf("Enter 1st number :\n");
+    scanf("%d",&num1);
+    
+    printf("Enter 2nd number :\n");
+    scanf("%d",&num2);
+    
+    mul=num1*num2;
+    
+    printf("Addition = %d",mul);
+    
+    return 0;
+}
