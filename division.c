4d82f1678987e4348c2c35a42cc3c497f9c43e58
Atharva Ghag <77572796+Atharvaghag@users.noreply.github.com>
2021-03-22 14:15:10 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,24 @@
+/*
+    Programme to divide 2 numbers
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int num1=0;
+    int num2=0;
+    int div=0;
+    
+    printf("Enter 1st number :\n");
+    scanf("%d",&num1);
+    
+    printf("Enter 2nd number :\n");
+    scanf("%d",&num2);
+    
+    div=num1/num2;
+    
+    printf("Addition = %d",div);
+    
+    return 0;
+}
